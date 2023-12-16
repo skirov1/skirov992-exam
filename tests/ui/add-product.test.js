f64b5dedcdb0ea2b88fc7182824f7a3093ef4bf6
Simeon Kirov <simeonkirov21@gmail.com>
2023-12-16 14:33:01 +0200

    Added ui tests to workflow

--- tests/ui/add-product.test.js
+++ tests/ui/add-product.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check add products page', async ({ page }) => {
-    await page.goto('http://localhost:8080/add-product');
+    await page.goto('https://skirov992-exam.onrender.com/add-product');
     const form = await page.$('form');
     expect(form).toBeTruthy();
   });
